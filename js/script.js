d3d526aee3694fd91c1ff2967a06d38c4282a74f
parsa-tahavori <parsa-tahavori@users.noreply.github.com>
2022-10-26 22:59:51 +0000

    Prettified Code!

--- js/script.js
+++ js/script.js
@@ -4,20 +4,22 @@
 // Created on: Mar 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+"use strict"
 /**
- * This function calculates the the pay and government payment 
+ * This function calculates the the pay and government payment
  */
-function calculate () {
+function calculate() {
   // input
-  const hoursWorked = parseInt(document.getElementById("number--of-hours-worked").value)
+  const hoursWorked = parseInt(
+    document.getElementById("number--of-hours-worked").value
+  )
   const hourlyRate = parseInt(document.getElementById("hourly-rate").value)
   // process
-  const pay = (hoursWorked * hourlyRate) * (1.00 - 0.18)
-  const taxes = (hoursWorked * hourlyRate) * 0.18
+  const pay = hoursWorked * hourlyRate * (1.0 - 0.18)
+  const taxes = hoursWorked * hourlyRate * 0.18
   // output
   document.getElementById("payment").innerHTML =
-    "Your pay will be: " + ' $' + pay.toFixed(2)
+    "Your pay will be: " + " $" + pay.toFixed(2)
   document.getElementById("income-tax").innerHTML =
-    "The government will take: " + ' $' + taxes.toFixed(2)
+    "The government will take: " + " $" + taxes.toFixed(2)
 }
